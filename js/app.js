a9c6be6bbc16cb22fb0e9c03ae2f97b685c848f4
Kevanthony <Michael_Curry99@outlook.com>
2022-06-10 23:12:41 -0500

    branch5

--- js/app.js
+++ js/app.js
@@ -104,11 +104,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  multiply(multArr[0], multArr[1], multArr[2]);
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+multiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
